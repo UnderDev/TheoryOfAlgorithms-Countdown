fd0fe8c8e6dbb1c8fec933f9b4342559a98c9839
UnderDev <scottcoyne@hotmail.com>
2017-02-21 15:27:21 +0000

    Code Clean up

--- CountDown Numbers Game.rkt
+++ CountDown Numbers Game.rkt
@@ -2,6 +2,7 @@
 
 ;() Empty List
 
+;--------------------------------------> Inital Setup <--------------------------------------------
 ; Defining the List of Small Numbers
 (define sNums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10))
 
@@ -15,22 +16,17 @@
 ; Defining the List of Choosen Numbers
 (define choosenNums (list 5 25))
 
-;---------> Random Number Function <----------
-;Function Generate a Random Number In the Range 101 - 999 *Not Sure is 101/999 can be picked
+;Random Number Function Takes in Two Numbers and returns a number in that range
 (define (rnd-num a b)
   (random a b))
-
 ;OR
-'(rnd-num 101 999)
-
-;Generate a Random Number In the Range 101 - 999 *Not Sure is 101/999 can be picked
-(random 101 999)
-
+;(rnd-num 101 999)
 
 ; Defining The Target Number
 (define targetNum (rnd-num 101 999))
 
-;Print out the lists
+
+;--------------------------------------> Print out the lists <--------------------------------------------
 "Small Number Lists"
 sNums
 
@@ -49,3 +45,20 @@ targetNum
 
 
 
+
+
+
+
+
+
+; oper is the operation to apply to the function elements.
+; id is the identity for oper (when used with oper nothing changes).
+(define (f oper id l)
+  (if (null? l)
+      id
+      (oper (car l) (f oper id (cdr l)))))
+
+; Now define fsum, which does the same thing as sum above, but define it using f.
+; Note that f takes three arguments, and all fsum does is hard-code two of those.
+(define (fsum l) (f + 0 l))
+(fsum (list 1 2 3 4 5))
