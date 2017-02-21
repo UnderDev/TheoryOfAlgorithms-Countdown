4c6e4679c296852d4fcdd31ba16ea46a9fd637d2
UnderDev <scottcoyne@hotmail.com>
2017-02-21 15:15:53 +0000

    Target Number Random

--- CountDown Numbers Game.rkt
+++ CountDown Numbers Game.rkt
@@ -11,23 +11,41 @@
 ; Defining the List of Large Numbers
 (define operators (list - + / *))
 
+
 ; Defining the List of Choosen Numbers
 (define choosenNums (list 5 25))
 
-;Target Number Between 101 and 999
-;Print out the lists
-sNums
-lNums
-operators
-choosenNums
+;---------> Random Number Function <----------
+;Function Generate a Random Number In the Range 101 - 999 *Not Sure is 101/999 can be picked
+(define (rnd-num a b)
+  (random a b))
 
-(define (random-choice a b)
-  (if (zero? (random 4)) a b))
-
-(random-choice 101 999)
-
+;OR
+'(rnd-num 101 999)
 
 ;Generate a Random Number In the Range 101 - 999 *Not Sure is 101/999 can be picked
 (random 101 999)
+
+
+; Defining The Target Number
+(define targetNum (rnd-num 101 999))
+
+;Print out the lists
+"Small Number Lists"
+sNums
+
+"Large Numbers List"
+lNums
+
+"Operators List"
+operators
+
+"Choosen Numbers List"
+choosenNums
+
+"TargetNum:"
+targetNum
+
+
 
 
