e43c6412de73221a39ef5aecdebe3041e44f085d
UnderDev <scottcoyne@hotmail.com>
2017-02-21 14:09:50 +0000

    Basic Lists Set up

--- CountDown Numbers Game.rkt
+++ CountDown Numbers Game.rkt
@@ -1,3 +1,18 @@
 #lang racket
 
+;() Empty List
 
+; Defining the List of Small Numbers
+(define sNums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10))
+
+; Defining the List of Large Numbers
+(define lNums (list 25 50 75 100))
+
+; Defining the List of Large Numbers
+(define operators (list - + / *))
+
+
+;Print out the lists
+sNums
+lNums
+operators
