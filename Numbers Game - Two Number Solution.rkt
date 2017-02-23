c156f676c66a338dea38b6c8f4c0bc9ed067411c
UnderDev <scottcoyne@hotmail.com>
2017-02-23 17:38:53 +0000

    Solution Idea for 3

--- Numbers Game - Two Number Solution.rkt
+++ Numbers Game - Two Number Solution.rkt
@@ -4,8 +4,14 @@
 (define ops (list + - * /))
 (define a 5)
 (define b 25)
-(define t 125)
+(define c 50)
+(define d 10)
+(define e 7)
+(define f 3)
 
+
+
+(define t 125)
 
 (define numsPicked (list 5 25))
 
@@ -20,99 +26,83 @@
 	(/ a b)
 	(/ b a)
 ))
-
 eval
 
 ; Recursion - Defining a function that you can pass in an operator and a list
 ; If The Null List? Return the List otherwise 1 - Do the following
 ; Begin alows you to do 3 things (3 functions etc)
 
-
-
-(define (f oper l)
+(define (fun oper l)
   (if (null? l)
      1
      (begin
        (display l)
        (display oper)      
        (newline)
-       ((car oper)(car l)(f(cdr oper)(cdr l))))))
+       ((car oper)(car l)(fun(cdr oper)(cdr l))))))
 
 ; 
-'(f ops numsPicked)
+'(fun ops numsPicked)
 
-
-
-(define (add x1 y1)
-      (+ x1 y1))
-(add 5 7)
-
-
-
-
-;SOLVE THE 2 nUMBER GAME ,
-;THAN MAP THAT TO THE FUNCTION
-; map takes 2 arg only
-;The Function + a list
-
+;Addition
 (define (plus l)
   (if (null? l)
       0
-      (+ (car l) (plus (cdr l)))))
+      (begin
+       (display l)
+      (+ (car (car l)) (plus (cdr (cdr l)))))))
 
+;Addition2
+(define (plus2 l)
+  (if (null? l)
+      0
+      (+ (car l) (plus2 (cdr l)))))
+
+
+;Multiply 
 (define (mult l)
   (if (null? l)
-      1
-      (* (car l) (plus (cdr l)))))
+      0
+      (* (car l) (mult (cdr l)))))
 
+;Divide 
 (define (divide l)
   (if (null? l)
       0
-      (/ (car l) (plus (cdr l)))))
+      (/ (car l) (divide (cdr l)))))
 
+;Minus
 (define (minus l)
   (if (null? l)
       0
-      (- (car l) (plus (cdr l)))))
+      (begin
+       (display l)
+      (- (car (car l)) (minus (cdr (cdr l)))))))
 
-(permutations (list a b))
-(combinations (list a b) 2)
+#|
+   a = 5 , b = 25 , c = 50
+   combinations = ((5 25) (5 50) (25 50))
+   permutations = ((5 25 50) (25 5 50) (5 50 25) (50 5 25) (25 50 5) (50 25 5))
+  (permutations (combinations (list a b c) 2)) =
+  (((5 25)  (5 50)  (25 50))
+  ( (5 50)  (5 25)  (25 50))
+  ( (5 25)  (25 50) (5 50))
+  ( (25 50) (5 25)  (5 50))
+  ( (5 50)  (25 50) (5 25))
+  ( (25 50) (5 50)  (5 25)))
+|#
+
+'(combinations (list a b c d e f) 2)
+(permutations (combinations (list a b c) 2))
+'(permutations (list a b c d e f))
+
+;Maps all the combinations
+'(map minus (permutations(list a b)))
+'(map mult  (permutations(list a b)))
+'(map divide(permutations(list a b)))
+
+'(map plus2  (combinations (list a b c d) 2))
+'(map minus (permutations (combinations (list a b c d) 2)))
 
 
-;Maps all the combinations 
-(map plus  (permutations(list a b)))
-(map minus (permutations(list a b)))
-(map mult  (permutations(list a b)))
-(map divide(permutations(list a b)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-; The two functions above look very similar.
-; Define a generic function with the pattern (from sum and mult).
-; oper is the operation to apply to the function elements.
-; id is the identity for oper (when used with oper nothing changes).
-;(define (g oper id l)
-  ;(if (null? l)
-      ;id
-      ;(begin
-      ;(display l)
-     ; (newline)
-      ;(oper (car l) (g  oper id (cdr l))))))
-
-; Now define fsum, which does the same thing as sum above, but define it using f.
-; Note that f takes three arguments, and all fsum does is hard-code two of those.
-;(define (fsum l) (g  + 0 l))
-
-; Test fsum out - same result as sum.
-;(fsum (list 1 2 3 4 5))
+'(list->set (map minus (permutations(list a b))))
