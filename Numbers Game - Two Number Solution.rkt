10163ce2c415d5c0d0174f545c4f1a3b3c987390
UnderDev <scottcoyne@hotmail.com>
2017-02-23 21:15:45 +0000

    Cartesian-product
    2 Numbers Attempt with Cartesian-product not working

--- Numbers Game - Two Number Solution.rkt
+++ Numbers Game - Two Number Solution.rkt
@@ -4,8 +4,8 @@
 (define ops (list + - * /))
 (define a 5)
 (define b 25)
-(define c 50)
-(define d 10)
+(define c 10)
+(define d 50)
 (define e 7)
 (define f 3)
 
@@ -13,10 +13,10 @@
 
 (define t 125)
 
-(define numsPicked (list 5 25))
+(define numsPicked (list 5 25 10))
 
 ; Hard Coded List of Posible Solutions
-(define eval (list
+(define evall (list
 	(+ a b)
 	(+ b a)
 	(- a b)
@@ -26,7 +26,7 @@
 	(/ a b)
 	(/ b a)
 ))
-eval
+evall
 
 ; Recursion - Defining a function that you can pass in an operator and a list
 ; If The Null List? Return the List otherwise 1 - Do the following
@@ -50,13 +50,15 @@ eval
       0
       (begin
        (display l)
-      (+ (car (car l)) (plus (cdr (cdr l)))))))
+      (+ (car l) (plus (cdr l))))))
 
 ;Addition2
 (define (plus2 l)
   (if (null? l)
       0
-      (+ (car l) (plus2 (cdr l)))))
+      (begin
+      (display l)
+      (+ (car (car l)) (plus2 (cdr l))))))
 
 
 ;Multiply 
@@ -80,29 +82,63 @@ eval
       (- (car (car l)) (minus (cdr (cdr l)))))))
 
 #|
-   a = 5 , b = 25 , c = 50
+   a = 5 , b = 25 , c = 10
    combinations = ((5 25) (5 50) (25 50))
    permutations = ((5 25 50) (25 5 50) (5 50 25) (50 5 25) (25 50 5) (50 25 5))
   (permutations (combinations (list a b c) 2)) =
-  (((5 25)  (5 50)  (25 50))
-  ( (5 50)  (5 25)  (25 50))
-  ( (5 25)  (25 50) (5 50))
-  ( (25 50) (5 25)  (5 50))
-  ( (5 50)  (25 50) (5 25))
-  ( (25 50) (5 50)  (5 25)))
+  (5 25)  (5 10)  (25 10)
+
+
+(combinations(permutations (list a b c) ))
 |#
 
 '(combinations (list a b c d e f) 2)
-(permutations (combinations (list a b c) 2))
-'(permutations (list a b c d e f))
+'(permutations (combinations (list a b c) 2))
+'(permutations (combinations(list a b c) 2))
 
 ;Maps all the combinations
+'(combinations(list numsPicked) 2)
+'(permutations(list numsPicked))
+
+
+;(cartesian-product (list * - + /) ( list 5 25 10)  (list 5 25 10))
+
+(define posEval (cartesian-product (list 1 2) (list 4 3)))
+
+posEval
+
+
+;( +(car(car posEval)) (car (car (cdr posEval))))
+
+
+(define (plussss l)
+  (if (null? l)
+      1
+      (begin
+       (display l)
+      ;(+ (car l)) (plussss (car (cadr l)))))))
+       (+ (car (car l)) (plussss (cadr(car l)))))))
+      ;( +(car(car l)) (car (cadr l))))))
+
+;first item in the list and the first value + first item in the list and cdr of the first value
+( + (car(car posEval)) (cadr (car posEval)))
+;(car (cdr (car posEval)))
+;(car (car posEval))
+
+
+ posEval
+;(map plussss (list posEval))
+
+
+
+
 '(map minus (permutations(list a b)))
 '(map mult  (permutations(list a b)))
 '(map divide(permutations(list a b)))
+
 
 '(map plus2  (combinations (list a b c d) 2))
 '(map minus (permutations (combinations (list a b c d) 2)))
 
 
-'(list->set (map minus (permutations(list a b))))
+'(list->set (map plus (permutations(list a b))))
