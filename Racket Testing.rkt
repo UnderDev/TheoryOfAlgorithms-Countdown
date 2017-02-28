b9e28ae2deaf1d2ba9af08e93e9a7928abbfe53d
UnderDev <scottcoyne@hotmail.com>
2017-02-28 16:00:07 +0000

    Optimization on 2 numbers

--- Racket Testing.rkt
+++ Racket Testing.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 ; Define The List etc
-(define ops (list + - * /))
+(define ops '(list + - * /))
 (define a 5)
 (define b 25)
 (define c 10)
@@ -152,7 +152,7 @@ posEval
 ;(car(posval) ns)
 
 
-;Atempted Recursion on list +using eval to evaluate them
+;Atempted Recursion on list + using eval to evaluate them
 (define (evalListRec l a)
   (if (null? l)
       a
@@ -165,6 +165,99 @@ posEval
 (define p (stuff posEval))
 
 ;Call the function and evaluate
-p
+'p
+
+
+
+;============================= Custom Cartisian-product ============================
+;(define (X . sets)
+;  (if (null? sets) '(())
+;      (let ((tails (apply X (cdr sets))))
+;        (apply append
+;               (map (lambda (h)
+;                      (map (lambda (t) (cons h t)) tails))
+;                    (car sets))))))
+;
+;(define lst (X ops numsPicked numsPicked))
+;(define (stuffff l )(evalListRec lst null))
+;
+;;
+;(define ccc (stuffff lst))
+;ccc
+;;Not Correct but looks promising
+;;(define posEval (cartesian-product '(* - + /) ( list 5 25 10)  (list 5 25 10)))
+;;(X ops posEval posEval)
+;===================================================================================
+
+'(eval (car posEval) ns)
+
+;(= (modulo 10 4) 2)
+
+
+
+;;Function below is n power n each loop
+;;Gets all the negitive numbers
+;(define (evalCond l a)
+;  (if (null? l)
+;      a
+;      (begin
+;       (display l)
+;      (evalCond (cdr l)(cons (eval (car l) ns) a)))
+;    ))
+
+
+
+
+;;Gets all the negitive numbers
+;(define (evalCond l a)
+;  (if (null? l)
+;      a
+;      (begin
+;       (display l)
+;    (if (positive? (eval (car l) ns))
+;        (evalCond (cdr l) a)
+;      (evalCond (cdr l)(cons (eval (car l) ns) a)))
+;    )))
+
+;(and (= 2 2) (> 2 1)) 
+
+
+
+;Atempted Recursion on list + using eval to evaluate them
+(define (evalCond l a)
+  (cond ((null? l) a)
+        '((positive? (eval (car l) ns)) (evalCond (cdr l)(cons (eval (car l) ns) a)))
+        ((integer? (eval (car l) ns)) (evalCond (cdr l)(cons (eval (car l) ns) a)))
+        (else (evalCond (cdr l) a))))
+
+
+
+
+;(define (evalCond l a)
+;  (if (null? l)
+;      a
+;      (and (if (positive? (eval (car l) ns)) (integer? (eval (car l) ns))
+;          (evalCond (cdr l)(cons (eval (car l) ns) a)))
+;          (evalCond (cdr l) a)
+;      )))
+
+
+
+
+   ; Both Working 100%
+   ; (if (integer? (eval (car l) ns))
+   ; (if (positive (eval (car l) ns))
+
+
+;New function passing in a list, and calling anoter function 
+(define (stuffCond l )(evalCond l null))
+
+;
+(define tester (stuffCond posEval))
+
+;Call the function and evaluate
+tester
+
+
 
 
