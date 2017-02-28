96a61b2ab0372e284c6de9e9bf6f1e7f05f9a6e4
UnderDev <scottcoyne@hotmail.com>
2017-02-28 16:12:31 +0000

    Working if and function on List

--- CountDown Numbers Game.rkt
+++ CountDown Numbers Game.rkt
@@ -2,7 +2,7 @@
 
 ;() Empty List
 
-;--------------------------------------> Inital Setup <--------------------------------------------
+;=================================>  Inital Setup <=================================================================
 ; Defining the List of Small Numbers
 (define sNums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10))
 
@@ -16,6 +16,9 @@
 ; Defining the List of Chosen Numbers
 (define ChosenNums (list 5 25 10))
 
+;Define The NameSpace for Eval
+(define ns (make-base-namespace))
+
 ;Random Number Function Takes in Two Numbers and returns a number in that range
 (define (rnd-num a b)
   (random a b))
@@ -28,7 +31,7 @@
 (define targetNum (rnd-num 101 999))
 
 
-;--------------------------------------> Print out the lists <--------------------------------------------
+;=================================> Print out the lists <===========================================================
 "Small Number Lists"
 sNums
 
@@ -43,21 +46,37 @@ ChosenNums
 
 "TargetNum:"
 targetNum
+;====================================================================================================================
+
 
 
 ;Cartesian cartesian-product gets all permemtations of the lists passed in 
 (define posEval (cartesian-product '(* - + /) ( list 5 25 10)  (list 5 25 10)))
 
 
-;Define The NameSpace for Eval
-(define ns (make-base-namespace))
-
+;=================================> Eval On List of Pos Combinations <===============================================
 ;Recursion On List of posible combinations
 ;Then Evaluate them and return a new list
-(define (evalListRec l a)
+'(define (evalListRec l a)
   (if (null? l)
       a
       (evalListRec (cdr l)(cons (eval (car l) ns) a))))
+;====================================================================================================================
+
+
+
+;=================================> Eval On List of Pos Combinations Optimized <=====================================
+;Pass in a list and an empty list,
+;check each item in the list and evaluate the result, adding it to the empty list only
+;IF its an integer and a positive number
+
+(define (evalListRec l a)
+  (cond ((null? l) a)
+        ((and (integer? (eval (car l) ns)) (positive? (eval (car l) ns))) (evalListRec (cdr l)(cons (eval (car l) ns) a)))
+        (else (evalListRec (cdr l) a))))
+
+;====================================================================================================================
+
 
 
 ;Defines a function that takes in a list and a function,
@@ -67,13 +86,6 @@ targetNum
 
 ;Call The Function And Pass in the List
 (evaluateList posEval)
-
-
-
-
-
-
-
 
 
 
