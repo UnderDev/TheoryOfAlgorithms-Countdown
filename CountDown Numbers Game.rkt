95b222aeb174044e78101731ca1fb1fc0a0a80fd
UnderDev <scottcoyne@hotmail.com>
2017-04-13 16:11:44 +0100

    Game Working Partially
    Dosnt get every Permutation / Combination, therfore answers arnt always
    found

--- CountDown Numbers Game.rkt
+++ CountDown Numbers Game.rkt
@@ -16,7 +16,7 @@
 (define (pick-item l)
   (list-ref l (random (length l))))
 
-(pick-item(list 3 4 5 6 7)) ;Working But need to do several times
+;(pick-item(list 3 4 5 6 7)) ;Working But need to do several times
 
 ;Maby get a random number in the range 1-10 for small numbers
 
@@ -31,86 +31,55 @@
   (random a b))
 
 ; Defining The Target Number (randdom number Between 101 and 999)
-'(define targetNum (rnd-num 101 999))
-
-; Cartesian cartesian-product gets all permemtations of the lists passed in 
-(define posEval (remove-duplicates (cartesian-product '(* - + /) ( list 5 25)  (list 5 25))))
-
-; Temp Target Number For Testing
-(define targetNum 30)
-
-;=================================> Print out the lists <=====================================================
-"Small Number Lists"
-sNums
-
-"Large Numbers List"
-lNums
-
-"Operators List"
-operators
-
-"Chosen Numbers List"
-ChosenNums
-
-"TargetNum:"
-targetNum
+(define targetNum (rnd-num 101 999))
 
 
-
-;=================================> Handy Things To Keep In Mind <============================================
+;=================================> Handy Things To Keep In Mind <===============================
 
 ;(remove-duplicates posEval) ;Removes duplicates from a list
 ;(random 101 999) ;Random Num Between 101 999
 ;(modulo 10 4)) ; gets the Moduls of 2 numbers
-
-;=================================> Eval On List of Pos Combinations <========================================
-;Recursion On List of posible combinations
-;Then Evaluate them and return a new list
-;(define (evalListRec l a)
-  ;(if (null? l)
-      ;a
-      ;(evalListRec (cdr l)(cons (eval (car l) ns) a))))
+;(list-tail lst 2) gets the remaining items from the list after pos 2
+;(andmap positive? (list 1 2 3 -8) checks every item on the list to see if positive this returns #f
 
 
-;=================================> Eval On List of Pos Combinations Optimized <==============================
+
+;=================================> Two Numbers Solved & Optimized <==============================
+
+; Cartesian cartesian-product gets all permemtations of the lists passed in 
+(define posEval (remove-duplicates (cartesian-product '(* - + /) ( list 5 25)  (list 5 25))))
+
+(define trgtNum 125)
+
 ;Pass in a list and an empty list,
 ;check each item in the list and evaluate the result, adding it to the empty list only
 ;IF its an integer and a positive number
-
 (define (evalListRec l a)
   (cond ((null? l) a)
         ((and (integer? (eval (car l) ns)) (positive? (eval (car l) ns))) (evalListRec (cdr l)(cons (eval (car l) ns) a)))
         (else (evalListRec (cdr l) a))))
 
 
-;==> Attempt 2 Match all with target number <==
+;==> Match all with target number <==
 (define (evalCond l a)
   (cond ((null? l) a)
-        ((and (integer? (eval (car l) ns)) (positive? (eval (car l) ns)) (equal? targetNum (eval (car l) ns))) (evalCond (cdr l)(cons (car l)  a)))
+        ((and (integer? (eval (car l) ns)) (positive? (eval (car l) ns)) (equal? trgtNum (eval (car l) ns))) (evalCond (cdr l)(cons (car l)  a)))
         (else (evalCond (cdr l) a))))
 
-
-
 ;Defines a function that takes in a list and a function,
-;passing that list into the function aswell 
 (define (evaluateList l )(evalCond l null))
 
-;Remove Duplicates from the List of pos combinations
-;(define solutions (remove-duplicates posEval))
-"Pos Solutions duplicates removed"
-;solutions
-posEval
 ;Call The Function evaluateList And Pass in the List
-;Use an if here to check if empty list, then display message
-"Solutions Shown Below For Target"
-targetNum
+"Target"
+trgtNum
+
+
+(writeln "Solution for 2 Numbers")
 (evaluateList posEval)
+;-------------------------------------------------------------------------------------------------
 
 
-
-
-
-;=================================> Pos Solution Six Numbers <==============================
+;=================================> Pos Solution Six Numbers <====================================
 ;Copyed From https://rosettacode.org/wiki/24_game/Solve#Racket
 ; Dosnt Work For All Numbers
 ; Eg goal 879,  Numbers '(2 7 9 10 50 25) "Solution Should be 879 = (2×(7+(9×50)))-(10+25)"
@@ -132,8 +101,6 @@ targetNum
              `(,n1 ,o1n ((,n2 ,o3n ,n3) ,o2n ,n4))
              `(,n1 ,o1n (,n2 ,o2n (,n3 ,o3n ,n4))))))))
 
-
-
 (define (find-solutions numbers (goal 173))
   (define in-operations (list + - * /))
   (remove-duplicates
@@ -147,14 +114,9 @@ targetNum
                [(res expr) (in-variants n1 o1 n2 o2 n3 o3 n4)]
                #:when (= res goal))
      expr)))
-
  (define (remove-from numbers . n) (foldr remq numbers n))
 
 ;(find-solutions '(2 7 9 10 50 25))
-
-
-
-
 
 ;=================================> Calculate Reverse Polish Notation <==============================
 ;Adapted From https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm
@@ -169,12 +131,16 @@ targetNum
      [('* (list x y s ___)) (cons (* x y) s)]
      [('/ (list x y s ___)) (cons (/ y x) s)]
      ))))
+;(calculate-RPN '(10 * * 2 / 7 + 3 - 8 50));Example 
 
 
-'(calculate-RPN '(10 * * 2 / 7 + 3 - 8 50));Example 
 
 
-;=================================> Build Pos RPN List <==============================
+
+
+
+
+;=================================> Build RPN List <==============================
 ;Function - Gets a random item from the list passed in
 (define (rand-element l)
   (list-ref l (random (length l))))
@@ -182,28 +148,24 @@ targetNum
 ; Gets the permutaions and Removes all duplicates from the list
 (define permu (remove-duplicates (permutations (list '- '+ '/ '*  3 7 2 10))))
 
+
 ;Appends the 2 numbers at the start to the list passed in and a random operator at the end
 (define (rpn l)
-  (append (list 50  8) l (list(rand-element operators))))
+  (append (shuffle(list 50 8)) l (list(rand-element operators))))
 
 ;maps the list to the function 
 (define (pos-Solu lst)(map rpn permu))
 ;(pos-Solu permu)
 
 
-(length (pos-Solu permu))
-
-
-(define target 646)
-
-;=================================> Check and Calculate RPN <==============================
-; Steps Involved in function below:
+;=================================> Check & Calculate RPN <==============================
+; Steps Involved in function below
 ; 1) Take in a list called e, and 2 optional lists.
 ; 2) Check if the list is null
 ; 3) If the list is null, Check dose it = the target number and return the equation, ELSE return #f
 ; 4) If the list is not null, Check the car of the list for the Following:
 ;
-;      (a) Is it a number? if yes add it to the stack and recall the function with the cdr of the list, also add it to the valid equation list.
+;      (a) Is it a number? if yes add it to the stack and re-call the function with the cdr of the list, also add it to the valid equation list.
 ;
 ;      (b) Is the car of the list a Procedure? AND the car of the stack a integer. (With error handling)
 ;            If TRUE, call the function again with the cdr of the list, then check the following:
@@ -220,36 +182,35 @@ targetNum
 
 (define (is-valid-rpn3? e [s '()] [eq '()])
   (begin
-    ;(printf "~a\t -> ~a~N \n" e s);Prints out the List and Stack
+   ;(printf "~a\t -> ~a~N \n" e s);Prints out the List and Stack
     (if (null? e)
-        (if (equal? (car s) target)
-        (reverse eq); or return s (The Stack or answer)
-        #f)
+        ;(if (equal? (car s) target)
+        #f
+        ;(reverse eq)
+        ;#f)
         (cond
-          [(number? (car e))(is-valid-rpn3? (cdr e) (cons (car e) s) (cons (car e) eq))]
-          [(with-handlers ([exn:fail? (lambda (exn) #f)])
-             (and (procedure? (eval(car e) ns)) (integer? (cadr s) ))
-             (is-valid-rpn3? (cdr e)
-                             (cond
-                               [(> (length s) 2) ((lambda (x y) (reverse(list y x))) (eval ((eval(car e)ns) (cadr s) (car s)) ns) (last s))]
-                               [(equal? (length s) 2) (list(eval ((eval(car e)ns) (cadr s) (car s)) ns))]
-                               [else (list #f)])
-                             (cons (car e) eq)))]
+          [(and (not (null? s)) (equal? (first s) targetNum) (reverse eq))]
+          [(and (number? (car e)) (andmap number? s))(is-valid-rpn3? (cdr e) (cons (car e) s) (cons (car e) eq))]
+          [(and (procedure? (eval(car e) ns)) (andmap real? s)  (andmap positive? s) (andmap number? s) (andmap integer? s))
+           (is-valid-rpn3? (cdr e)
+                           (cond
+                             [(> (length s) 2) (flatten((lambda (x y) (reverse(list y x))) (eval ((eval(car e)ns) (cadr s) (car s))ns) (list-tail s 2)))]
+                             [(equal? (length s) 2) (list(eval ((eval(car e)ns) (cadr s) (car s)) ns))]
+                             [else (list #f)])
+                           (cons (car e) eq))]
           [else #f]))))
 
 ;Testing only 
-(define templst3 (remove-duplicates(permutations (list 1 2 3 '+ '-))))
-;(filter identity(filter identity(map is-valid-rpn3? templst3)))
+(define templst3 (remove-duplicates(permutations (list 2 8 3 '/ '*))))
+;(remove-duplicates (filter identity(filter identity(map is-valid-rpn3? templst3))))
 
-;All Permutations of a List size 5 = 120
-;templst3
+\newline
+"Target To Reach"
+targetNum
 
-;(filter identity(map is-valid-rpn3? templst3))
-(filter identity(map is-valid-rpn3? (pos-Solu permu)));9187 pos answers of valid reverse polish notation
-
-
-
-
+\newline
+"Solution's for full Game"
+(remove-duplicates (filter identity(map is-valid-rpn3? (pos-Solu permu))))
 
 
 
